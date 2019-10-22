721879af445f1a5e3849815f4dfff3918e6a9b11
lee_honleon@wistron <lee_honleon@163.com>
2019-10-22 16:00:53 +0800

    nginx提交

---
+++ run-nginx.sh
@@ -0,0 +1,2 @@
+#!bin/bash
+docker run  --name wing-nginx -d -p 80:80 -p 443:443 -v /ZriproWing/wing-main/CmsWing:/usr/share/nginx/html -v /ZriproWing/nginx/nginx.conf:/etc/nginx/nginx.conf -v /ZriproWing/nginx/conf.d:/etc/nginx/conf.d  -v /ZriproWing/nginx/logs:/var/log/nginx -v /ZriproWing/nginx/cert:/etc/nginx/cert/ nginx
